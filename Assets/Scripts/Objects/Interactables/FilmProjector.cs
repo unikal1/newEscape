d635e83cdaedb52c84ad9b83d4ac269404fa62e4
dioo1461 <dioo1461@gmail.com>
2024-05-27 20:46:48 +0900

    Feat: 프로젝터 작동 중에 상호작용 불가하게 변경, 프로젝터 조명과 영상 프로젝터 꺼져있을때 보이지 않게 수정

--- Assets/Scripts/Objects/Interactables/FilmProjector.cs
+++ Assets/Scripts/Objects/Interactables/FilmProjector.cs
@@ -3,49 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 
-public class FilmProjector : BaseInteractiveObj
-{
-    [SerializeField] VideoPlayer videoPlayer;
-    public VideoPlayer VideoPlayer { get => videoPlayer; set => videoPlayer = value; }
-    [SerializeField]
-    bool _haveBattery = false;
-    public bool HaveBattery { get => _haveBattery; set => _haveBattery = value; }
-    public override void Interact()
-    {
-        base.Interact();
+public class FilmProjector : BaseInteractiveObj {
+	[SerializeField] VideoPlayer videoPlayer;
+	public VideoPlayer VideoPlayer { get => videoPlayer; set => videoPlayer = value; }
+	[SerializeField] bool _haveBattery = false;
+	[SerializeField] Canvas videoCanvas;
+	[SerializeField] Light spotLight;
+	public bool HaveBattery { get => _haveBattery; set => _haveBattery = value; }
 
-        if (!_haveBattery)
-        {
-            for (int i = 0; i < Managers.Data.InventoryData.itemDatas.Count; i++)
-            {
-                if (Managers.Data.InventoryData.itemDatas[i].ItemType == DataDefine.EItemType.Battery)
-                {
-                    Managers.Data.InventoryData.itemDatas.RemoveAt(i);
-                    ((UI_GameScene)Managers.Scene.CurrentScene.SceneUI).UI_Inventory.UpdateInventory();
-                    _haveBattery = true;
-                    videoPlayer.Play();
-                    Managers.Sound.Play("Sounds/Objects/projector-activate-1");
-                    break;
-                }
-            }
-        }
-        else
-        {
-            videoPlayer.Play();
-            Managers.Sound.Play("Sounds/Objects/projector-activate-1");
-        }
-    }
-       
-    // Start is called before the first frame update
-    void Start()
-    {
-        if(videoPlayer == null)
-            videoPlayer = GetComponentInChildren<VideoPlayer>();
-    }
+	public override void Interact() {
+		base.Interact();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		if (!_haveBattery) {
+			Managers.Sound.Play("Sounds/Objects/projector-button-click-1");
+			for (int i = 0; i < Managers.Data.InventoryData.itemDatas.Count; i++) {
+				if (Managers.Data.InventoryData.itemDatas[i].ItemType == DataDefine.EItemType.Battery) {
+					Managers.Data.InventoryData.itemDatas.RemoveAt(i);
+					((UI_GameScene)Managers.Scene.CurrentScene.SceneUI).UI_Inventory.UpdateInventory();
+					_haveBattery = true;
+					videoCanvas.enabled = true;
+					Managers.Sound.Play("Sounds/Objects/projector-activate-1");
+					StartCoroutine(PlayVideoCoroutine());
+					break;
+				}
+			}
+		} else {
+			Managers.Sound.Play("Sounds/Objects/projector-button-click-1");
+			Managers.Sound.Play("Sounds/Objects/projector-activate-1");
+			StartCoroutine(PlayVideoCoroutine());
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start() {
+		if (videoPlayer == null)
+			videoPlayer = GetComponentInChildren<VideoPlayer>();
+
+		// 비디오 플레이어를 초기화하고 비디오를 정지 상태로 설정합니다.
+		videoPlayer.playOnAwake = false;
+		videoPlayer.Stop();
+		spotLight.enabled = false;
+		videoCanvas.enabled = false;
+	}
+
+	IEnumerator PlayVideoCoroutine() {
+		gameObject.tag = "unInteractable";
+		spotLight.enabled = true;
+		videoCanvas.enabled = true;
+		videoPlayer.Play();
+		yield return new WaitForSeconds((float)videoPlayer.clip.length);
+		videoPlayer.Stop();
+		videoCanvas.enabled = false;
+		spotLight.enabled = false;
+		gameObject.tag = "interactable";
+	}
 }
